2a7bf6421e4f9ec7dc9c67529562c23b17845545
Trevor Reimer <trevor@wallsmetalroofing.com>
2021-09-29 17:48:48 -0300

    Start setting up the WMR app api endpoints. Created the authorization class for the api. Added the create and update product endpoint.

---
+++ wmr-app-link/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': "WMR App Link",
+    'version': '0.1',
+    'summary': 'Walls Metal Roofing App Link',
+    'application': False,
+    'installable': True,
+    'data': []
+}
---
+++ wmr-app-link/controllers/controller.py
@@ -0,0 +1,77 @@
+import json
+import re
+
+from odoo import SUPERUSER_ID, http, models
+from odoo.http import request
+from werkzeug.exceptions import BadRequest
+
+
+# Authenticate the api request using the odoo api keys
+class IrHttp(models.AbstractModel):
+    _inherit = "ir.http"
+
+    @classmethod
+    def _auth_method_wmr_api_key(cls):
+        api_key = request.httprequest.headers.get("Authorization")
+        if not api_key:
+            raise BadRequest(
+                "Authorization header with API key missing")
+
+        # sanitize the bearer token to retrieve the api key
+        api_key = re.sub(r'bearer\s', '', api_key, 1, re.IGNORECASE)
+        user_id = request.env["res.users.apikeys"]._check_credentials(
+            scope="rpc", key=api_key
+        )
+
+        # check that the user id is set
+        if not user_id:
+            raise BadRequest("API key invalid")
+
+        request.uid = user_id
+
+
+# Create the primary api entry point
+class ApiController(http.Controller):
+
+    # Listen for product create requests
+    @http.route("/wmr/product/create", auth="wmr_api_key", type="json")
+    def product_create(self, request):
+        print("Create Product")
+
+        user = request.params['user']
+        product = request.params['product']
+
+        res = request.env['product.product'].with_user(
+            user['id']
+        ).create([product])
+        print(res)
+
+        return {
+            'success': True,
+            'product': {
+                'id': res.id
+            }
+        }
+
+    # Listen for product create requests
+    @http.route("/wmr/product/update", auth="wmr_api_key", type="json")
+    def product_update(self, request):
+        print("Update Product")
+
+        user = request.params['user']
+        product = request.params['product']
+        product_id = product['id']
+        del product['id']
+
+        # Get and update the record details
+        record = request.env['product.product'].browse([product_id])
+        record.with_user(
+            user['id']
+        ).update(product)
+
+        return {
+            'success': True,
+            'product': {
+                'id': product_id
+            }
+        }
