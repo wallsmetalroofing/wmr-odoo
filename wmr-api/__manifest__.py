de741c4e8f528e8faee39479dde4c34c467d66ce
radhey-wmr <radhey@wallsmetalroofing.com>
2023-05-10 16:55:44 -0300

    Made views and form for sub-contact modules

--- wmr-api/__manifest__.py
+++ wmr-api/__manifest__.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "wmr-api",
-
-    'summary': """
+    "name": "wmr-api",
+    "summary": """
         This Modules Contains All the APIs used for creating and updating the values of following:
         1.Sales""",
-
-    'description': """
+    "description": """
         1. Sales
             a. Create API (URL: /wmr-api/sales/create): This API creates the sales order and add the first quote to it.
                 Note: The user ID must be passed in the request so that any data changes are made would be registered on the name of that user.
@@ -76,19 +74,24 @@
                     }
                 }
     """,
-
-    'author': "Radhey",
-    'website': "http://www.wallsmetalroofng.com",
-
+    "author": "Radhey",
+    "sequence": -100,
+    "website": "http://www.wallsmetalroofng.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Customization',
-    'version': '0.1',
-
+    "category": "Customization",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-    'data': ['views/contact_view.xml',],
-    'installable': True,
-    'auto_install': False
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/contact_view.xml",
+        "views/contact_form.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+    "license": "LGPL-3",
 }
