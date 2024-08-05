6427d9ad95d5f03f62b5ed227e346ebae2c65c62
Radhey Rupapara <radhey@wallsmetalroofing.com>
2024-08-05 10:39:30 -0400

    Reviewed the pull request

--- wmr-app-link/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': "WMR App Link",
-    'version': '0.1',
-    'summary': 'Walls Metal Roofing App Link',
-    'application': False,
-    'installable': True,
-    'depends': ["sale_mrp", "stock"],
-    'data': []
-}
